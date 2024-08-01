33a5c09d7baca7b998bae4c138f0336be375a3e6
Marharyta Skokava <marharyta.skokava@sap.com>
2024-08-01 17:02:02 +0200

    remove properties

--- src/main/java/com/margarita/quiz/controller/QuizQuestionController.java
+++ src/main/java/com/margarita/quiz/controller/QuizQuestionController.java
@@ -1,11 +1,14 @@
 package com.margarita.quiz.controller;
 
+import com.margarita.quiz.model.QuizQuestion;
 import com.margarita.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RequiredArgsConstructor
 @RequestMapping("/quiz")
@@ -20,12 +23,12 @@ public class QuizQuestionController {
     }
 
     @GetMapping("/questions/random")
-    public ResponseEntity<String> receiveRandomQuestion() {
+    public ResponseEntity<QuizQuestion> receiveRandomQuestion() {
         final var question = quizService.findRandomQuestion();
-        return ResponseEntity.ok(question.get().toString());
+        return ResponseEntity.ok(question.get());
     }
 
-    @PostMapping("/questions")
+    @PostMapping(value = "/questions", consumes = {MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String> saveQuestions(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
--- src/main/java/com/margarita/quiz/service/MdFileParser.java
+++ src/main/java/com/margarita/quiz/service/MdFileParser.java
@@ -1,12 +1,14 @@
 package com.margarita.quiz.service;
 
 import com.margarita.quiz.model.QuizQuestion;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -28,12 +30,25 @@ public class MdFileParser {
             if (summaryMatcher.find()) {
                 String summary = summaryMatcher.group(1).trim();
                 String content = detailBlock.substring(summaryMatcher.end()).trim();
+                String htmlContent = mdIntoHtml(content);
                 var quizQuestion = new QuizQuestion();
                 quizQuestion.setQuestion(summary);
-                quizQuestion.setAnswer(content);
+                quizQuestion.setAnswer(htmlContent);
+                quizQuestion.setNextDateTime(LocalDateTime.now());
                 return quizQuestion;
             } else return null;
         }).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public static String mdIntoHtml(String mdText) {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        // Parse the Markdown text into a Document
+        Node document = parser.parse(mdText);
+
+        // Render the Document to HTML
+        return renderer.render(document);
     }
 
 }
